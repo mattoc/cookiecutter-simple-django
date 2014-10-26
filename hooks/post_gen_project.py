9e51337dcf3a71eba9f1c7cc02fcc40d6d5fa920
Matt Stevenson <mattoc@mattoc.com>
2014-10-26 18:59:13 +1100

    Auto-create git repo and save initial buildout

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,4 +3,12 @@
 
 import subprocess
 
+# Grant user exec permissions on manage.py
 subprocess.call(['chmod', 'u+x', 'manage.py'])
+
+# Start git repository for project
+subprocess.call(['git', 'init'])
+
+# Save initial commit
+subprocess.call(['git', 'add', '.'])
+subprocess.call(['git', 'commit', '-m"Initial commit"'])
